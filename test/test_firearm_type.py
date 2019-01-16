1d9219ec2bc84f1ea3fecbb7bf2ebdeb1b7f4d6c
prewittdavon <davonprewitt@lawn-128-61-58-105.lawn.gatech.edu>
2019-01-16 15:57:23 -0500

    Adds testing stubs

---
+++ test/test_firearm_type.py
@@ -0,0 +1,29 @@
+import unittest
+
+#from main import classify
+
+class TestFirearmType(unittest.TestCase):
+    def test_firearm_unpresent():
+
+    def test_firearm_hand():
+
+    def test_firearm_rifle():
+
+    def test_firearm_machine():
+
+    def test_firearm_toy():
+
+    def test_firearm_water():
+
+    def test_firearm_flare():
+
+    '''
+    Example of test setup
+    --------
+    def test_sum(self):
+        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+
+    '''
+
+if __name__ == '__main__':
+    unittest.main()
