1dae2bff727839ff64a382808e8281474c174f78
prewittdavon <davonprewitt@lawn-128-61-62-22.lawn.gatech.edu>
2019-02-06 14:31:48 -0500

    Adds video feed methods

--- model.py
+++ model.py
@@ -14,20 +14,16 @@ def transform_frame(frame):
     numpy_final = np.expand_dims(numpy_frame, axis=0)
     return numpy_final
 
-
 def live_feed():
     camera = cv2.VideoCapture(0)
     while(1):
         # Capture frame from camera
         ret, frame = camera.read()
         frame = cv2.resize(frame, (299, 299), interpolation=cv2.INTER_CUBIC)
-
         # Adhere to TS graph input structure
         numpy_final = transform_frame(frame)
-
         if (detect(numpy_final) > .85):
             # TODO(): Alert security personnel
-
         # Break from feed
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -35,13 +31,24 @@ def live_feed():
     camera.release()
     cv2.destroyAllWindows()
 
-def youtube_video_feed():
+def youtube_video_feed(link):
+    # Optional
 
-def image_search_feed():
+def google_image_search_feed():
+    # TODO(): use chromedriver extension
+    # https://github.com/hardikvasa/google-images-download#arguments
+
+def local_video_feed(filename):
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--feed', default='live')
     with tf.Session() as sess:
-        # Execute surveillance:
+        feed_
+
+        if parser.parse_args().threshold
+        # # Execute surveillance:
         # live_feed()
         # youtube_video_feed()
-        # image_search_feed()
+        # google_image_search_feed()
+        # local_video_feed()
