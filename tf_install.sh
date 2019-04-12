4c8c568812bfba8db50917129c8f413b664bdc3d
kkhosla97 <35663149+kkhosla97@users.noreply.github.com>
2019-04-12 15:01:52 -0400

    Updated script install locations from root to home, got rid of -y for pip installs
    v 1.2

--- tf_install.sh
+++ tf_install.sh
@@ -5,25 +5,25 @@
 # NOTE TO SELF: Installation instructions should include chmod for script and install for git
 # NOTE TO SELF: Installation instructions should include instructions on how to run the model
 
-cd /
+cd ~
 sudo apt-get update
 sudo apt-get install -y python3-dev python3-pip
 sudo apt-get install git
-pip3 install --ignore-installed --upgrade tensorflow==1.9 -y
-pip3 install -y pillow==5.4.1 lxml==4.3.1 jupyter==1.0.0 matplotlib==3.0.2 opencv-python
+pip3 install --ignore-installed --upgrade tensorflow==1.9
+pip3 install pillow==5.4.1 lxml==4.3.1 jupyter==1.0.0 matplotlib==3.0.2 opencv-python
 
-cd /
-sudo mkdir /TensorFlow
-cd /TensorFlow
+cd ~
+sudo mkdir ~/TensorFlow
+cd ~/TensorFlow
 git clone https://github.com/tensorflow/models.git
 cd models/research
 sudo wget https://github.com/protocolbuffers/protobuf/releases/download/v3.7.1/protoc-3.7.1-linux-x86_64.zip
 sudo unzip protoc-3.7.1-linux-x86_64.zip
-sudo export PYTHONPATH=$PYTHONPATH:/TensorFlow/models/research/object_detection
-sudo export PYTHONPATH=$PYTHONPATH:/TensorFlow/models/research:/TensorFlow/models/research/slim
-sudo source ~/.bashrc
+export PYTHONPATH=$PYTHONPATH:/TensorFlow/models/research/object_detection
+export PYTHONPATH=$PYTHONPATH:/TensorFlow/models/research:/TensorFlow/models/research/slim
+source ~/.bashrc
 ./bin/protoc object_detection/protos/*.proto --python_out=.
 
-cd /TensorFlow/models/research/
+cd ~/TensorFlow/models/research/
 sudo python3 setup.py build
 sudo python3 setup.py install
